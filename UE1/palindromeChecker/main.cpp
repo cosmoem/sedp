a40b97a0fe9fa4912560187ce95c716c79f86b48
Frieda Gruse <fr.ida@gmx.de>
2021-04-25 18:43:53 +0200

    fixed palindrome checker to lowercase

--- UE1/palindromeChecker/main.cpp
+++ UE1/palindromeChecker/main.cpp
@@ -7,16 +7,17 @@
 #include <chrono>
 #include <fstream>
 
-
+// Function to check if a string is a palindrome
 bool isPalindrome(std::string word){
     int wordSize= word.size();
+    std::for_each(word.begin(), word.end(), [](char & c){
+        c = ::tolower(c);
+    });
     if (wordSize%2==0) {
-
         std::string partOne = word.substr(0, wordSize / 2 );
         std::string partTwo = word.substr(wordSize / 2, wordSize-1);
         reverse(partTwo.begin(),partTwo.end());
         return partOne == partTwo;
-
     }
     else{
         std::string partOne = word.substr(0, wordSize / 2);
@@ -26,19 +27,15 @@ bool isPalindrome(std::string word){
     }
 }
 
-
-
 int main(int argc, char* argv [])
 {
 
     // Input
-
-    std::ifstream cin("../palindromes_small.txt");
+    std::ifstream cin("../palindrome_mini.txt");
     std::streambuf *cinbuf = std::cin.rdbuf();
     std::cin.rdbuf(cin.rdbuf());
 
-
-   std::string line;
+    std::string line;
     std::vector<std::string> lines;
     while (std::getline(std::cin, line))
     {
@@ -52,26 +49,23 @@ int main(int argc, char* argv [])
         line.reserve(size);
     }
 
-
-    // Todo: Implement algorithm to sort palindromes to the front of the list
-
+    // Check input for palindromity
     std::vector<std::string> palindromeVector;
     std::vector<std::string> nonPalindromeVector;
     for(const auto & word :lines)
     {
-
         if(isPalindrome(word)){
             palindromeVector.push_back(word);
         }else{
             nonPalindromeVector.push_back(word);
         }
-
     }
 
+    // Overwrite lines vector so that it's sorted
     std::copy(palindromeVector.begin(),palindromeVector.end(), lines.begin());
-    std::copy(nonPalindromeVector.begin(),nonPalindromeVector.end(), lines.begin()+ palindromeVector.size());
+    std::copy(nonPalindromeVector.begin(), nonPalindromeVector.end(), lines.begin() + palindromeVector.size());
 
-
+    // Output
     std::cout << "Sorted by palindromity" << std::endl << std::endl;
     for (const auto & s : lines)
     {
