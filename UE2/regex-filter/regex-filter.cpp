cd4eeaec61cc9d525013236943d35afc367074b1
Frieda Gruse <fr.ida@gmx.de>
2021-05-09 23:09:53 +0200

    regex solved

--- UE2/regex-filter/regex-filter.cpp
+++ UE2/regex-filter/regex-filter.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <regex>
+#include <map>
 
 using namespace std::string_literals;
 
@@ -46,18 +48,40 @@ int main(int argc, char* argv[])
 
         lines.push_back(std::move(line));
     }
-    
-    // Todo: filter
-    
-    // Todo: count
-    
-    // Todo: output
 
-    for (const auto & line : lines) {
-        std::cout << line << '\n';
+    // Filtering and counting
+
+    std::regex regex(argv[1]); // Command-line argument regex
+
+    // Case-sensitive per default
+    if(argc >= 3) {
+        if(argv[2]) { // Command-line argument to set case-insensitive TODO is there a specific command-line switch option?
+            regex.assign(argv[1], std::regex_constants::icase);
+        }
     }
 
-    std::cout.flush();
+    std::smatch base_match;
+    std::map<std::string, int> matches;
+    for (const auto & line : lines) {
+        // discarding all unmatched lines
+        if(std::regex_match(line, base_match, regex)) {
+            // collecting and counting the match within the line
+            if (base_match.size() == 2) {
+                matches[base_match[1].str()] += + 1;
+            }
+        }
+    }
+
+    // Outputting all matches with their number of occurrences
+    for (auto const & match : matches) {
+        std::cout << match.first << ": " << match.second << std::endl;
+    }
     
     return 0;
 }
+
+// compile: g++ -std=c++14 -o regex-filter regex-filter.cpp
+// run case-sensitive option 1: ./regex-filter "Lu.*@(hpi|student.*).de" --> no results
+// run case-sensitive option 2: ./regex-filter "Lu.*@(hpi|student.*).de" false --> no results
+// run case-insensitive: ./regex-filter "Lu.*@(hpi|student.*).de" true --> hpi: 1, student.hpi.uni-potsdam: 2
+// run case-sensitive: ./regex-filter "lu.*@(hpi|student.*).de" --> hpi: 1, student.hpi.uni-potsdam: 2
