a6654a6c99388ccbcfa2ef6790111b0239e97294
Frieda Gruse <fr.ida@gmx.de>
2021-05-10 01:31:13 +0200

    runner/ program failure immunity

--- UE2/runner/runner.cpp
+++ UE2/runner/runner.cpp
@@ -2,16 +2,27 @@
 #include <string>
 #include <iostream>
 #include <signal.h>
+#include <cstdlib>
+#include <algorithm>
 #include "externallibrary.h"
 
+// signal handlers
 void signal_handler(int sig) {
+    static int count{0};
+    std::string sigName = sys_signame[sig];
+    std::transform(sigName.begin(), sigName.end(), sigName.begin(), ::toupper);
     switch (sig) {
-        case SIGFPE:
-            std::cout << "Received SIGFPE " << sig << " - Floating Point Exception..." << std::endl;
+        // SIGKILL and SIGSTOP cannot be handled, ignored or blocked --> no handling needs to be defined
+        // should terminate the program at the second occurrence using exit(int)
+        case SIGINT:
+            if (count == 0) { std::cout << "Received SIG" << sigName << " " << sig << " - IGNORING AND CONTINUING..." << std::endl; }
+            else { std::cout << "Received SIG " sigName << " " << sig << " for the second time - EXITING NOW" << std::endl; exit(1); }
             break;
-        case SIGABRT:
-            std::cout << "Received SIGABRT " << sig << " - Abort Trap..." << std::endl;
-            break;
+        // other signals defined by C and C++ should get ignored if permitted by the standard TODO use SIG_IGN ???
+        default:
+            std::cout << "Received SIG" << sigName << " " << sig << " - IGNORING AND CONTINUING..." << std::endl; break;
+        // should be terminated using quick_exit(int) if not possible to ignore TODO ????
+        // The program should not terminate in the case of other signals or thrown exceptions (those declared by C and C++ are sufficing) TODO ??????
     }
 }
 
@@ -22,6 +33,7 @@ int main(int argc, char * argv[])
 {
     signal(SIGFPE, (*signal_handler));
     signal(SIGABRT, (*signal_handler));
+    signal(SIGINT, (*signal_handler));
 
     bool dryRun = atoi(argv[1]) < 1000000;
     initialize(dryRun);
@@ -32,6 +44,7 @@ int main(int argc, char * argv[])
         try {
             processEvents();
         }
+        // exception handler
         catch(std::out_of_range::exception) {
             std::cout << "Caught Out Of Range Exception..." << std::endl;
             break;
