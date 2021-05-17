dfb1e5e3fe508c00d2698b7e6681f22bf2502a4e
Frieda Gruse <fr.ida@gmx.de>
2021-05-17 20:21:01 +0200

    graph with shared pointers

--- UE2/graph/Graph.h
+++ UE2/graph/Graph.h
@@ -27,18 +27,18 @@ private:
  */
 class Edge {
 public:
-    Edge(Vertex * v0, Vertex * v1, std::int32_t weight);
+    Edge(std::shared_ptr<Vertex> v0, std::shared_ptr<Vertex> v1, std::int32_t weight);
 
-    void reset(Vertex * v0, Vertex * v1);
+    void reset(std::shared_ptr<Vertex> v0, std::shared_ptr<Vertex> v1);
 
     std::int32_t weight() const;
-    Vertex * v0() const;
-    Vertex * v1() const;
+    std::shared_ptr<Vertex> v0() const;
+    std::shared_ptr<Vertex> v1() const;
 
 private:
     std::int32_t m_weight;
-    Vertex * m_v0;
-    Vertex * m_v1;
+    std::shared_ptr<Vertex> m_v0;
+    std::shared_ptr<Vertex> m_v1;
     InstanceCounter<Edge> m_instanceCounter;
 };
 
@@ -48,22 +48,31 @@ private:
  */
 class Graph {
 public:
-    void addVertex(Vertex * vertex);
-    void addEdge(Edge * edge);
+    /*
+    Graph(Graph && other)  noexcept
+    : m_vertices(std::move(other.m_vertices))
+    , m_edges(std::move(other.m_edges))
+    , m_instanceCounter(other.m_instanceCounter) {
 
-    const std::vector<Vertex *> & vertices() const;
-    const std::vector<Edge *> & edges() const;
+    }*/
+
+    void addVertex(std::shared_ptr<Vertex> vertex);
+    void addEdge(std::shared_ptr<Edge> edge);
+
+    const std::vector<std::shared_ptr<Vertex>> & vertices() const;
+    const std::vector<std::shared_ptr<Edge>> & edges() const;
 
     /*
      * @postcondition g2 is empty, i.e., does not contain any vertices or edges
      */
-    void merge(Graph * g2);
+    // TODO post condition isn't met
+    void merge(std::shared_ptr<Graph> g2);
     Graph * minimumSpanningTree() const;
 
     friend std::ostream& operator<<(std::ostream& stream, const Graph & graph);
 
 private:
-    std::vector<Vertex *> m_vertices;
-    std::vector<Edge *> m_edges;
+    std::vector<std::shared_ptr<Vertex>> m_vertices;
+    std::vector<std::shared_ptr<Edge>> m_edges;
     InstanceCounter<Graph> m_instanceCounter;
 };
