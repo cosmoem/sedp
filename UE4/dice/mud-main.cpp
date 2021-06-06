aa81758c77a50628e6b4d7e3a6cf4086bf689079
Frieda Gruse <fr.ida@gmx.de>
2021-06-06 17:25:32 +0200

    framework UE4

---
+++ UE4/dice/mud-main.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+
+
+int main(int argc, char * argv[])
+{
+    const auto printUsage = [argv] { std::cout << "usage: " << argv[0] << " <num players> <num rounds>" << std::endl; };
+    if (argc != 3)
+    {
+        printUsage();
+        return EXIT_FAILURE;
+    }
+
+    auto numPlayers = 0;
+    auto numRounds = 0;
+    try
+    {
+        numPlayers = std::stoul(argv[1]);
+        numRounds = std::stoul(argv[2]);
+    }
+    catch (std::exception & e)
+    {
+        std::cerr << e.what() << std::endl;
+        printUsage();
+        return EXIT_FAILURE;
+    }
+    
+    // TODO: create game
+    
+    // TODO: create players
+    
+    // TODO: simulate game
+    
+    // TODO: output results
+}
