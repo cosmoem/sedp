604179ea1a1a8f4d86c1be38448db5379478e371
Frieda Gruse <fr.ida@gmx.de>
2021-06-15 17:34:42 +0200

    4.2

--- UE4/beziercurve/CubicBezierCurve.h
+++ UE4/beziercurve/CubicBezierCurve.h
@@ -1,87 +1,64 @@
 
 #pragma once
 
-#include <vector>
-#include <map>
-#include "Point.h"
-
-class CubicBezierCurve {
-public:
-    // TODO init with 0
-//    CubicBezierCurve() {
-//
-//    };
-
-    CubicBezierCurve(std::initializer_list<std::initializer_list<double>> lists);
-
-    Point operator()(double t) const;
-
-    // TODO: copy constructor does not work for whatever reason -> so use assignment constructor
-    CubicBezierCurve &operator=(const CubicBezierCurve &other) {
-        this->controlPoints = other.controlPoints;
-        this->cache = other.cache;
-        return *this;
-    }
-
-    void setControlPoint(int index, Point controlPoint) {
-        controlPoints[index] = controlPoint;
-    }
-
+class Point {
 private:
-    std::vector<Point> controlPoints;
-    std::map<double, double> cache;
+    std::array<double, 3> coordinates;
+public:
+    Point() : coordinates{0,0,0} {}
+    Point(double x, double y, double z) : coordinates{x, y, z} {}
 
-    int binomialCoefficient(int n, int k) const {
-        int res = 1;
+    double operator[](int index) const {
+        return coordinates[index];
+    }
 
-        // Since C(n, k) = C(n, n-k)
-        if (k > n - k)
-            k = n - k;
+    friend Point operator*(double value, Point point) {
+        return Point(
+            value*point.coordinates[0],
+            value*point.coordinates[1],
+            value*point.coordinates[2]
+        );
+    }
 
-        // Calculate value of
-        // [n * (n-1) *---* (n-k+1)] / [k * (k-1) *----* 1]
-        for (int i = 0; i < k; ++i) {
-            res *= (n - i);
-            res /= (i + 1);
-        }
-
-        return res;
+    Point& operator+=(Point point) {
+        this->coordinates[0] = (point.coordinates[0]+this->coordinates[0]);
+        this->coordinates[1] = (point.coordinates[1]+this->coordinates[1]);
+        this->coordinates[2] = (point.coordinates[2]+this->coordinates[2]);
+        return *this;
     }
 };
 
-Point CubicBezierCurve::operator()(double t) const {
-    // here werden die values returned :)
-    // caching noch einbauen
-    // und was ist wenn cache nicht genau trifft
-    double x = 0;
-    double y = 0;
-    double z = 0;
-    for (int i = 0; i < 4; i++) {
-        x += binomialCoefficient(3, i) * std::pow((1-t), 3-i) * std::pow(t, i) * controlPoints[i][0];
-        y += binomialCoefficient(3, i) * std::pow((1-t), 3-i) * std::pow(t, i) * controlPoints[i][1];
-        z += binomialCoefficient(3, i) * std::pow((1-t), 3-i) * std::pow(t, i) * controlPoints[i][2];
+class CubicBezierCurve {
+public:
+    CubicBezierCurve() = default;
+    explicit CubicBezierCurve(Point p0, Point p1, Point p2, Point p3) : controlPoints{p0, p1, p2, p3} {}
+    CubicBezierCurve(CubicBezierCurve const &curve) = default;
+    CubicBezierCurve(CubicBezierCurve &&curve) = default;
+
+    void setControlPoint(int index, Point p) {
+        controlPoints[index] = p;
     }
 
-    return {x, y, z};
-}
-
-CubicBezierCurve::CubicBezierCurve(std::initializer_list<std::initializer_list<double>> lists) {
-    // TODO: there must be an easier way to do this shit
-    std::array<double, 3> temp{};
-    for (auto list : lists) {
-        std::copy(list.begin(), list.end(), temp.begin());
-        controlPoints.emplace_back(temp);
+    Point operator()(double t) const {
+        Point curvePoint = Point();
+        for (int i = 0; i <= 3; i++) {
+            Point p = binomialCoefficient(3, i) * std::pow(1 - t, 3 - i) * std::pow(t, i) * controlPoints[i];
+            curvePoint += p;
+        }
+        return curvePoint;
     }
 
-    // calculate cache
-    /*
-     *
-     */
-//    for (auto t = 0.0; t <= 1.0; t += 1.0 / 256.0) {
-//        cache[t] =
-//    }
-}
+private:
+    std::array<Point, 4> controlPoints;
 
-//CubicBezierCurve::CubicBezierCurve() {
-//
-//}
+    // https://www.geeksforgeeks.org/binomial-coefficient-dp-9/
+    static int binomialCoefficient(int n, int k) {
+        if (k > n) {
+            return 0;
+        }
+        if (k == 0 || k == n) {
+            return 1;
+        }
+        return binomialCoefficient(n - 1, k - 1) + binomialCoefficient(n - 1, k);
+    }
+};
