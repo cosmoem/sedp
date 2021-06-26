10ff7ced272e06a15de45f86b9ad093d30208ae9
Fabian Georgi <fabian.georgi@student.hpi.uni-potsdam.de>
2021-06-26 19:07:39 +0200

    changes by Frieda

--- UE5/signal/Signal.h
+++ UE5/signal/Signal.h
@@ -4,3 +4,30 @@
 #include <iostream>
 #include <functional>
 #include <iostream>
+
+template<typename... T>
+class Signal {
+public:
+    Signal() {
+        callbacks_ = {};
+    };
+
+    void connect(std::function<void(T...)> callback) {
+        this->callbacks_.push_back(callback);
+    };
+
+    template<typename Z>
+    void connect(Z object, void (Z::* const mf)(T...)) {
+        auto x = [&object, mf](auto... args) { return (object.*mf)(args...); };
+        this->callbacks_.push_back(x);
+    };
+
+    void emit(T... values) {
+        for (auto callback : callbacks_) {
+            callback(values...);
+        }
+    };
+
+private:
+    std::vector<std::function<void(T...)>> callbacks_;
+};
